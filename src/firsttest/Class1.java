5c207f1cfc6fdb559db7dd8e344923bea343dbf4
PatrikH1 <patrik.hulthen@live.com>
2019-08-31 01:11:24 +0200

    New files added.

---
+++ src/firsttest/Class1.java
@@ -0,0 +1,21 @@
+package firsttest;
+
+/**
+ * @author Patrik
+ *
+ */
+public class Class1 {
+
+	/**
+	 * 
+	 */
+	public Class1() {
+		System.out.println("Inside constructor Class1");	
+	}
+	
+	public void message1() {
+		System.out.println("Message 1 from class 1");
+	}
+	
+
+}
